8d53f4a9baef4b30b2e576a9d561c0043dc13123
John Jorgensen <john@leprechaun.dev>
2024-09-01 15:05:47 -0600

    most basic prosemirror setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import {EditorState, Transaction} from "prosemirror-state";
+import {EditorView} from "prosemirror-view";
+
 export class LiveMark {
     targetElement: HTMLElement;
+    state: EditorState;
+    view: EditorView;
     constructor(targetElement: HTMLElement) {
         this.targetElement = targetElement;
+        this.state = EditorState.create({});
+        this.view = new EditorView(this.targetElement, {
+            state: this.state,
+            dispatchTransaction: this.transactionHandler,
+        });
+    }
+    transactionHandler(transaction: Transaction) {
+        const newState = this.view.state.apply(transaction);
+        this.view.updateState(newState);
     }
 }
