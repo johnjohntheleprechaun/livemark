97ca4395005f96a94f6d7b406116d1c3698015eb
John Jorgensen <john@leprechaun.dev>
2024-09-26 17:17:06 -0600

    move some input rules, and also make italic/bold styling one

--- src/input-rules.ts
+++ src/input-rules.ts
@@ -1,12 +1,35 @@
-import {inputRules, textblockTypeInputRule, wrappingInputRule} from "prosemirror-inputrules";
+import {InputRule, inputRules, textblockTypeInputRule, wrappingInputRule} from "prosemirror-inputrules";
 import {schema} from "prosemirror-markdown";
-
-const header = textblockTypeInputRule(/^(#{1,6})\s$/, schema.nodes.heading, match => ({level: match[1].length}));
-const blockquote = wrappingInputRule(/^>\s/, schema.nodes.blockquote);
+import {Transaction} from "prosemirror-state";
 
 export const livemarkInput = inputRules({
     rules: [
-        header,
-        blockquote,
+        textblockTypeInputRule(/^(#{1,6})\s$/, schema.nodes.heading, match => ({level: match[1].length})),
+        wrappingInputRule(/^>\s/, schema.nodes.blockquote),
+        textblockTypeInputRule(/^```$/, schema.nodes.code_block),
+        wrappingInputRule(/^\s*(-|\+|\*)\s/, schema.nodes.bullet_list),
+        wrappingInputRule(/^[0-9](?:\.|\))\s/, schema.nodes.ordered_list),
+        // live italic/bold
+        new InputRule(
+            /((?:\*|_){1,3})[^*_]+\1([^*_])/,
+            (state, match, start, end): Transaction => {
+                const tr = state.tr;
+                console.log(tr);
+
+                tr.delete(end - match[1].length, end);
+                tr.delete(start, start + match[1].length);
+                tr.insertText(match[2]);
+
+                // if it's odd, which is equivelant to saying if it's one or three
+                if (match[1].length % 2 === 1) {
+                    tr.addMark(start, end - match[1].length * 2, schema.marks.em.create());
+                }
+                if (match[1].length >= 2) {
+                    tr.addMark(start, end - match[1].length * 2, schema.marks.strong.create());
+                }
+
+                return tr;
+            }
+        ),
     ]
 });
