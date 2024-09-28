019c79fa02ec4d9b8ae0e1ca5d1baa8e49a26a4d
John Jorgensen <john@leprechaun.dev>
2024-09-27 21:59:35 -0600

    add live formatting (it's actually a lil broken but good progress still)

--- src/input-rules.ts
+++ src/input-rules.ts
@@ -14,19 +14,30 @@ export const livemarkInput = inputRules({
             /((?:\*|_){1,3})[^*_]+\1([^*_])?$/,
             (state, match, start, end): Transaction => {
                 const tr = state.tr;
-                console.log(tr);
+                console.log(match);
+                console.log(tr, start, end);
 
-                tr.delete(end - match[1].length, end);
-                tr.delete(start, start + match[1].length);
-                tr.insertText(match[2]);
+                let realEnd: number = end + 1;
+                if (match[2]) {
+                    // if you've fully finished then finalize it
+                    tr.delete(end - match[1].length, end);
+                    tr.delete(start, start + match[1].length);
+                    tr.insertText(match[2]);
+                    realEnd = end - 1 - match[1].length * 2;
+                }
+                else {
+                    tr.insertText(match[1][0]);
+                }
 
                 // if it's odd, which is equivelant to saying if it's one or three
                 if (match[1].length % 2 === 1) {
-                    tr.addMark(start, end - match[1].length * 2, schema.marks.em.create());
+                    tr.addMark(start, realEnd, schema.marks.em.create());
                 }
                 if (match[1].length >= 2) {
-                    tr.addMark(start, end - match[1].length * 2, schema.marks.strong.create());
+                    tr.addMark(start, realEnd, schema.marks.strong.create());
                 }
+
+                tr.setStoredMarks([]);
 
                 return tr;
             }
